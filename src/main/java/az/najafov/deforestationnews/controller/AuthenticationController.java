159948a17e2f0c3cf2f5022091f5bb89806cdec1
unajafov <ulvinajafov@ailab.az>
2024-02-12 14:56:53 +0400

    tags added to AuthenticationController

--- src/main/java/az/najafov/deforestationnews/controller/AuthenticationController.java
+++ src/main/java/az/najafov/deforestationnews/controller/AuthenticationController.java
@@ -5,6 +5,7 @@ import az.najafov.deforestationnews.dto.LoginRequestDto;
 import az.najafov.deforestationnews.dto.RegistrationRequestDto;
 import az.najafov.deforestationnews.dto.TokenResponseDto;
 import az.najafov.deforestationnews.service.AuthenticationService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
@@ -24,16 +25,19 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("/login")
+    @Operation(summary = "User Login", description = "Authenticates a user and provides an access token.")
     public GenericResponse<TokenResponseDto> login(@RequestBody @Valid LoginRequestDto loginRequestDto) {
         return GenericResponse.success(authenticationService.login(loginRequestDto), "SUCCESS");
     }
 
     @GetMapping("/refresh")
+    @Operation(summary = "Refresh Token", description = "Refreshes the user's access token using a refresh token.")
     public GenericResponse<String> refresh(HttpServletRequest request) {
         return GenericResponse.success(authenticationService.refresh(request), "SUCCESS");
     }
 
     @PostMapping("/register")
+    @Operation(summary = "User Registration", description = "Registers a new user with the provided details.")
     public GenericResponse<Void> register(@RequestBody @Valid RegistrationRequestDto requestDto) {
         authenticationService.register(requestDto);
         return GenericResponse.success("SUCCESS");
